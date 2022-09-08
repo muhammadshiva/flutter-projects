43f48f31b033c24338917448834f4dbd2e1a57ca
muhammadshiva <shivamuhammad07@gmail.com>
2022-09-08 13:30:21 +0700

    adding navigation controls to the Appbar

--- webview_in_flutter/lib/main.dart
+++ webview_in_flutter/lib/main.dart
@@ -1,41 +1,42 @@
-import 'dart:io';
+import 'dart:async'; // Add this import
+
 import 'package:flutter/material.dart';
-import 'package:webview_flutter/webview_flutter.dart';
-import 'package:webview_in_flutter/web_view_stack.dart';
+import 'package:webview_flutter/webview_flutter.dart'; // Add this import back
+
+import 'src/navigation_controls.dart'; // Add this import
+import 'src/web_view_stack.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    home: WebViewApp(),
-  ));
+  runApp(
+    const MaterialApp(
+      home: WebViewApp(),
+    ),
+  );
 }
 
 class WebViewApp extends StatefulWidget {
   const WebViewApp({Key key}) : super(key: key);
 
   @override
-  _WebViewAppState createState() => _WebViewAppState();
+  State<WebViewApp> createState() => _WebViewAppState();
 }
 
 class _WebViewAppState extends State<WebViewApp> {
-  // Enabling Hybrid Composition
-  @override
-  void initState() {
-    if (Platform.isAndroid) {
-      WebView.platform = SurfaceAndroidWebView();
-    }
-    super.initState();
-  }
+  final controller =
+      Completer<WebViewController>(); // Instantiate the controller
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Flutter Webview'),
+        title: const Text('Flutter WebView'),
+        // Add from here ...
+        actions: [
+          NavigationControls(controller: controller),
+        ],
+        // ... to here.
       ),
-      body: const WebViewStack(),
-      // body: const WebView(
-      //   initialUrl: 'https://flutter.dev',
-      // ),
+      body: WebViewStack(controller: controller), // Add the controller argument
     );
   }
 }
